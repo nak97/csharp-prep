5b1b749ad13020a908e70c6c8e3a2f961d8520e5
Nicholas Keen <nicknack2019@yahoo.com>
2023-09-16 19:05:59 -0600

    first commit

---
+++ Prep1/.history/Program_20230916184425.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine(first);
+
+    }
+}
